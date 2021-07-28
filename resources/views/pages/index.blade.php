1865971e4642bfd5db3f66874510abdd5ce0bdc1
Ice <ice@Ices-MacBook-Pro.local>
2021-07-28 10:20:26 -0400

    bayblade

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,158 @@
+@extends('layouts.master')
+@section('content')
+
+
+
+    <!-- Header -->
+    <header id="header" class="header" style="background-color:black;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="text-container">
+                        <h1 class="h1-large" style="color:white;">BayBlade Galleria Exclusive HighEnd NFT Club</h1>
+                        <p class="p-large" style="color:white;">BayBlade is a high-end collection of the most rarest Blades, each with a unique design and power level. Each Blade is specifically crafted and minted with unique abilities. There will only be 100 BayBlades minted on the block chain, we are an exclusive community and every blade is valuable.
+</p>
+                        <a class="btn-solid-lg" href="#your-link"><i class="fab fa-apple"></i>NFT Shop</a>
+                        <a class="btn-solid-lg secondary" style="color:white;" href="#your-link"><i class="fab fa-google-play"></i>Download</a>
+                    </div> <!-- end of text-container -->
+                </div> <!-- end of col -->
+                <div class="col-lg-6">
+                    <div class="image-container">
+                        <img class="img-fluid" src="images/cyberusred.gif" alt="alternative">
+                    </div> <!-- end of image-container -->
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </header> <!-- end of header -->
+    <!-- end of header -->
+
+
+    <!-- Introduction -->
+    <div class="basic-1" style="background-color:black;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p  style="color:white;">Each BayBlade NFT differs from the previous BayBlade in stats and other characteristic, minting only one copy of that specific bayblade for the general public.</p>
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of basic-1 -->
+    <!-- end of introduction -->
+
+
+    <!-- Features -->
+    <div id="features" class="cards-1"  style="background-color:black;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="img-fluid" src="badge.png" alt="alternative">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title" style="color:white;">Edition</h5>
+                            <p style="color:white;">There will be First and Second Edition BayBlade NFT's, Each Set Will Have 50 BayBlades Increasing The Rarity Of The Blade.</p>
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="img-fluid" src="images/features-icon-2.svg" alt="alternative">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title" style="color:white;">Type</h5>
+                            <p style="color:white;"> Each BayBlade Has A Unique Type That Will Give To It Rarity</p>
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-image">
+                            <img class="img-fluid" src="images/features-icon-3.svg" alt="alternative">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title" style="color:white;">Stats</h5>
+                            <p style="color:white;">Each BayBlade NFT Has Very Unique Stats To It That Drives Up The Value Of That BayBlade.</p>
+                        </div>
+                    </div>
+                    <!-- end of card -->
+
+               
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of cards-1 -->
+    <!-- end of features -->
+
+
+    <!-- Details 1 -->
+    <div id="details" class="basic-2" style="background-color:black;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="text-container">
+                        <h2 style="color:white">Welcome To The Club</h2>
+                        <p style="color:white;">When you buy a Bayblade NFT your buying a unique blade that will unlock different attributes in our community that only memebers will have access to, your BayBlade serves as your identity each blade is different and the combinations of Beast, Color, Edition, Specialty and Type. </p>
+                        <!-- <p>We enjoy helping small and medium sized tech businesses take a shot at established Fortune 500 companies</p> -->
+                    </div> <!-- end of text-container -->
+                </div> <!-- end of col -->
+                <div class="col-lg-7">
+                    <div class="image-container">
+                    <img class="img-fluid" src="images/cyberusyellow.gif" alt="alternative">
+                    </div> <!-- end of image-container -->
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of basic-2 -->
+    <!-- end of details 1 -->
+
+
+
+   
+
+    <!-- Details 2 -->
+    <div class="basic-3" style="background-color:black;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="image-container">
+                    <img class="img-fluid" src="images/cyberusred.gif" alt="alternative">
+                    </div> <!-- end of image-container -->
+                </div> <!-- end of col -->
+                <div class="col-lg-5">
+                    <div class="text-container">
+                        <h2 style="color:white">NFT RoadMap</h2>
+                        <ul class="list-unstyled li-space-lg">
+                            <li class="media">
+                                <i class="fas fa-chevron-right"></i>
+                                <div class="media-body" style="color:white">Currently Releasing First Edition BayBlade 50 Will Be Minted</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-chevron-right"></i>
+                                <div class="media-body" style="color:white">Once The First Edition Set is Complete, We Will Start On The Second Edition!</div>
+                            </li>
+                            <li class="media">
+                                <i class="fas fa-chevron-right"></i>
+                                <div class="media-body" style="color:white">Our Goal Is To Bring Value To These NFT's We Are Focused On Growing A Large Community Where We Can Bring Excitement And Rarity To NFT's.</div>
+                            </li>
+                        </ul>
+                        <a class="btn-solid-reg popup-with-move-anim" href="#details-lightbox">Buy NFT</a>
+                        <a class="btn-outline-reg" href="article.html">Twitter</a>
+                    </div> <!-- end of text-container -->
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of basic-3 -->
+    <!-- end of details 2 -->
+
+
+
+
+
+
+@endsection
